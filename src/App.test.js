e9741f429760ce1521e1c90838b89e696f339921
unknown <raleshir@gmail.com>
2022-03-10 18:58:56 -0800

    maybe fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders the WORD MASTER title', () => {
+test('renders the Linkdle title', () => {
   render(<App />)
-  const title = screen.getByText(/WORD MASTER/i)
+  const title = screen.getByText(/Linkdle/i)
   expect(title).toBeInTheDocument()
 })
